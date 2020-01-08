1e10dd75cee1970ab4a674615a362ae8fd8f01f9
sl131 <sl131@mail.ru>
2020-01-08 15:18:18 +0300

    first Setting

---
+++ CustomNavigationBar/CustomNavigationBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomNavigationBar
+//
+//  Created by sl131 on 08.01.2020.
+//  Copyright © 2020 sl131. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
